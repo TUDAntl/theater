b233965c263e89090bda7219954544ad4f986fe2
Ekin Uensay <ekin.uensay@gmail.com>
2018-10-23 21:49:13 +0200

    beta

--- client/main.js
+++ client/main.js
@@ -1,8 +1,134 @@
 import './main.html';
 import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+import { Chart } from 'chart.js';
 
-LatestSlots = new Mongo.Collection('slotCol');
 
-Template.slots.aaa = function(){
-  return LatestSlots.find().fetch();
-};
+LatestSlotsStaat = new Mongo.Collection('slotCol');
+LatestSlotsSide = new Mongo.Collection('slotCol2');
+
+Template.lastAlerts.helpers({
+  "getLastAlert": function(){
+    return LatestSlotsStaat.find({$or:[ {alert:"Emptying"}, {alert:"Filling"}]}, { limit:10, sort: {date: -1}} );
+  }
+})
+
+Template.slots.helpers({
+  "getFslots" : function(){
+    var selected = LatestSlotsStaat.find({}, { limit: 200, sort: {date: -1}}).fetch();
+    return selected;
+  }
+});
+
+Template.ChartStaat.onRendered(function () {
+  this.autorun(() => {
+    var myChartObject = document.getElementById("myChartStaat");
+
+    var rawObject = LatestSlotsStaat.find({}, { limit:10, sort:{date: -1}});
+    var collData = [];
+    rawObject.forEach(function(cat){
+      collData.push(cat.fslots);
+    });
+    var collLabels = [];
+    rawObject.forEach(function(cat){
+      collLabels.push([cat.time.substring(0,5)]);
+    });
+    var color;
+    if(collData[0]>=150){
+    color ='rgba(27,188,18,0.4)';
+    }
+    if(150>collData[0]&&collData[0]>=50){
+    color ='rgba(226,141,22,0.4)';
+    }
+    if(50>collData[0]){
+    color ='rgba(162,0,0,0.4)';
+    }
+
+    collData = collData.reverse();
+    collLabels = collLabels.reverse();
+    var chart = new Chart(myChartObject, {
+      type: 'line',
+      data: {
+        labels: collLabels,
+        datasets:[{
+          label: "Slots",
+          data: collData,
+          backgroundColor: color
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true,
+              userCallback: function(label, index, labels) {
+                // when the floored value is the same as the value we have a whole number
+                if (Math.floor(label) === label) {
+                  return label;
+                }
+
+              },
+            }
+          }],
+        },
+      }
+    });
+  })
+});
+
+/*Template.ChartSide.onRendered(function () {
+  this.autorun(() => {
+    var myChartObject = document.getElementById("myChartSide");
+
+    var rawObject = LatestSlotsSide.find({}, { limit:10, sort:{date: -1}});
+    var collData = [];
+    rawObject.forEach(function(cat){
+      collData.push(cat.fslots);
+    });
+    var collLabels = [];
+    rawObject.forEach(function(cat){
+      collLabels.push([cat.time.substring(0,5)]);
+    });
+    var color;
+    if(collData[0]>=150){
+    color ='rgba(27,188,18,0.4)';
+    }
+    if(150>collData[0]>=50){
+    color ='rgba(226,141,22,0.4)';
+    }
+    if(50>collData[0]){
+    color ='rgba(162,0,0,0.4)';
+    }
+
+    collData = collData.reverse();
+    collLabels = collLabels.reverse();
+    var chart = new Chart(myChartObject, {
+      type: 'line',
+      data: {
+        labels: collLabels,
+        datasets:[{
+          label: "Slots",
+          data: collData,
+          backgroundColor: color
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true,
+              userCallback: function(label, index, labels) {
+                // when the floored value is the same as the value we have a whole number
+                if (Math.floor(label) === label) {
+                  return label;
+                }
+
+              },
+            }
+          }],
+        },
+      }
+    });
+  })
+});
+*/
--- server/main.js
+++ server/main.js
@@ -1,29 +1,117 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo'
 
-const url = "https://svg.parkinghq.com/parking_cities/533801/parking_units/5338";
-LatestSlots = new Mongo.Collection('slotCol');
+const url = ["https://svg.parkinghq.com/parking_cities/533801/parking_units/5338",
+"https://svg.parkinghq.com/parking_units/6328",
+"https://www.q-park.de/de-de/städte/darmstadt/luisencenter/"
+];
+LatestSlotsStaat = new Mongo.Collection('slotCol');
+LatestSlotsSide = new Mongo.Collection('slotCol2');
+Archive = new Mongo.Collection('alerts');
+var currentStaat;
+var oldStaat;
+var currentSide;
+var oldSide;
+var archiveCd=50;
 
 
 Meteor.startup(() => {
-Meteor.setInterval(function(){Meteor.call("getSlots")}, 1000*60);
+  //LatestSlotsStaat.remove({});
+  Meteor.setInterval(function(){Meteor.call("getSlotsStaat"),
+  Meteor.call("writeArchive")
+}, 1000*5);
 });
 
 Meteor.methods({
-  getSlots(){
-    const result = HTTP.call('GET',url, function( error, response ) {
-  if ( error ) {
-    console.log( error );
-  } else {
+  getSlotsSide(){
+    var result = HTTP.call('GET',url[2], function( error, response ) {
+      if ( error ) {
+        console.log( error );
+      } else {
 
-    var cont = response.content;
-    var posStart = cont.indexOf("badge-low'>");
-    var posEnd = cont.indexOf("</span></p>")
-    var freeSlots = cont.slice(posStart+11,posEnd)
-    console.log("Free Slots:",freeSlots);
+        var time = (new Date).toTimeString().substring(0,8);
+        var date = new Date();
+        var cont = response.content;
+        var posStart = -1;
 
-    LatestSlots.insert({fslots : freeSlots, time : 111});
+        if(cont.indexOf("<span class=\"spots\">") != -1)
+        posStart = cont.indexOf("<span class=\"spots\">") + 20;
 
-  }});
+
+        var posEnd = cont.indexOf("Parkpl");
+        var freeSlots = cont.slice(posStart, posEnd);
+
+        var mesg = "-";
+        if(freeSlots!=currentSide){
+          if(currentSide > oldSide + 9){
+            mesg="Emptying Alert";
+          }
+          if(currentSide < oldSide -9){
+            mesg = "Filling Alert"
+          }
+          LatestSlotsSide.insert({fslots : freeSlots, time : time, date: date, alert: mesg});
+        }
+
+        if (typeof oldSide === "undefined") {
+          oldSide=currentSide;
+        }
+        oldSide = currentSide;
+        currentSide=freeSlots;
+        console.log("Side", currentSide, oldSide);
+      }});
+    },
+    getSlotsStaat(){
+      var result = HTTP.call('GET',url[0], function( error, response ) {
+        if ( error ) {
+          console.log( error );
+        } else {
+
+          var time = (new Date).toTimeString().substring(0,8);
+          var date = new Date();
+          var cont = response.content;
+          var posStart = -1;
+
+          if(cont.indexOf("<span class='badge badge-low") != -1)
+          posStart = cont.indexOf("<span class='badge badge-low") + 30;
+          if(cont.indexOf("<span class='badge badge-medium") != -1)
+          posStart = cont.indexOf("<span class='badge badge-medium") + 33;
+          if(cont.indexOf("<span class='badge badge-high") != -1)
+          posStart = cont.indexOf("<span class='badge badge-high") + 31;
+          if(cont.indexOf("<span class='badge badge-black") != -1)
+          posStart = cont.indexOf("<span class='badge badge-black") + 32;
+
+          var posEnd = cont.indexOf("</span></p>");
+          var freeSlots = cont.slice(posStart, posEnd);
+          //var freeSlots = time.substring(6,8);
+          var mesg = "-";
+
+
+          if(freeSlots!=oldStaat){
+            console.log("Staat", "Current:",freeSlots,"Old", oldStaat);
+
+            if(Number(freeSlots) > Number(oldStaat) + 9){
+              mesg="Emptying";
+            }
+            if(Number(freeSlots) + 9 < Number(oldStaat) ){
+              mesg = "Filling"
+            }
+            LatestSlotsStaat.insert({fslots : freeSlots, time : time, date: date, alert: mesg});
+          }
+
+          mesg="-";
+          oldStaat = freeSlots;
+
+        }});
+      },
+      writeArchive(){
+        /*var time = (new Date).toTimeString().substring(0,5);
+        if(archiveCd>0){
+        archiveCd--;
+      }
+      else if(time == "03:00"){
+      Archive.insert(LatestSlots);
+      LatestSlots.remove({});
+      archiveCd = 50;
+    }*/
   }
 })
